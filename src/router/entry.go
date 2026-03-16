be77a42bd6b052fc5c5e3d42e2b55a1b9803b308
talonops <227240021+talonops@users.noreply.github.com>
2026-03-16 15:06:51 -0400

    Update documentation route in entry.go: change the route for API documentation from an absolute path to a relative path for improved portability and consistency in file referencing.

--- src/router/entry.go
+++ src/router/entry.go
@@ -12,7 +12,7 @@ import (
 func Routes(app *fiber.App) {
 	v1 := app.Group("/v1", logger.New())
 	app.Get("/novnc/*", static.New("/home/talon/novnc-static"))
-	app.Get("/docs", static.New("/home/talon/getaced/docs/api.getaced.io-documentation.html"))
+	app.Use("/docs", static.New("./docs/api.getaced.io-documentation.html"))
 
 	// Public auth routes
 	v1.Get("/auth/google", handler.GoogleAuth)
